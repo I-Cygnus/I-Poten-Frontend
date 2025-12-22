531a365c16a5a9dc8aee9336d0cf2beb8ead5157
Hyunsoo Choi <gustn9025@naver.com>
2025-12-22 16:07:39 +0900

    Revert "update: [최현수] main 랜딩 페이지 및 마이페이지 재구성"
    This reverts commit 89c054d37211375d678b98487c6f99200efa10b9.

--- sveltekit-review-app/.__mf__temp/svelteKitReviewApp/localSharedImportMap.js
+++ sveltekit-review-app/.__mf__temp/svelteKitReviewApp/localSharedImportMap.js
@@ -1,7 +1,6 @@
 
 // Windows temporarily needs this file, https://github.com/module-federation/vite/issues/68
 
-    import {loadShare} from "@module-federation/runtime";
     const importMap = {
       
     }
