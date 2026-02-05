06fe145bb69988bb1a14c0937561f2d13839fd3b
Hyunsoo Choi <gustn9025@naver.com>
2026-02-05 15:08:19 +0900

    chore: [최현수] footer 이용약관 페이지 경로 추가

--- main-container/src/components/Footer.tsx
+++ main-container/src/components/Footer.tsx
@@ -126,7 +126,7 @@ export default function SiteFooter() {
         <TopSection>
           <Nav>
             <NavLink to="/">i-poten</NavLink>
-            <NavLink to="/">이용약관</NavLink>
+            <NavLink to="/vue-account/account/terms-of-service">이용약관</NavLink>
             <NavLink to="/">개인정보 처리방침</NavLink>
             <NavLink to="/">제휴문의</NavLink>
           </Nav>
